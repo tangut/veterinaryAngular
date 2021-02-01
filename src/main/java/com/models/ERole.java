39ad70dcfaeb6f6776e84cca928141775c76aaa1
Yaroslav <tangut1998@gmail.com>
2021-02-01 16:49:21 +0400

    added angular login and registration

---
+++ src/main/java/com/models/ERole.java
@@ -0,0 +1,5 @@
+package com.models;
+
+public enum ERole {
+    ROLE_USER, ROLE_ADMIN, ROLE_MODERATOR;
+}
---
+++ src/main/java/com/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.repository;
+
+import java.util.Optional;
+
+import com.models.ERole;
+import com.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+}
